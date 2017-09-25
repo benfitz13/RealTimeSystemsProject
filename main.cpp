5ffac185d6ef787b070af436a58336d5a6970008
benfitz13 <benfitz13@gmail.com>
2017-09-25 12:02:01 -0400

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//main.cpp
+#include <stdio.h>
+#include <iostream>
+#include "Movement.h"
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
+
+
+int main()
+{
+	Movement test;
+	
+	//show starting position for X,Y, and Z
+	test.showPosition();
+	
+	//Moves the X, Y and Z variables a ceratian number of places
+	//The Parameter is how many places you want to move the variables
+	test.moveXYZ(5);
+	
+	//displays the current location of X, Y, and Z
+	test.showPosition();
+	
+  
+	return 0;
+}
