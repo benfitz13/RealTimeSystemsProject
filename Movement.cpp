b4688d1931833ac98cc3dc29cb3f5de3d6998da4
Ben <benfitz13@gmail.com>
2017-09-27 11:59:57 -0400

    Update Movement.cpp

--- Movement.cpp
+++ Movement.cpp
@@ -1,7 +1,7 @@
-//Movement.cpp
 #include "Movement.h"
 #include <iostream>
 #include <fstream>
+#include <string.h>
 using namespace std;
 Movement::Movement()
 {
@@ -11,9 +11,9 @@ Movement::Movement()
 	currenty2 = 2;
 	currentz1 = 3;
 	currentz2 = 6;
-	x = 0;
-	y = 0;
-	z = 0;
+	x = 1;
+	y = 2;
+	z = 3;
 }
 
 Movement::Movement(int initial_currnetx1, int initial_currnetx2,
@@ -35,13 +35,10 @@ Movement::Movement(int initial_currnetx1, int initial_currnetx2,
 void Movement::moveXYZ(int distance)
 {
 
-	int counter = 0;
-	while (counter < distance)
+	int counter = 1;
+	while (counter < distance || counter == distance)
 	{
-		currentx1 ++;
-		currentx2 ++;
-		currenty1 ++;
-		currentz2 ++;
+	
 		if (currentx1 ==8)
 			currentx1 = 0;
 		if (currentx2 == 7)
@@ -50,18 +47,54 @@ void Movement::moveXYZ(int distance)
 			currenty1 = 0;
 		if (currentz2 ==7)
 			currentz2 = 0;
-		x = moveArray[currentx1][currentx2];
-		y = moveArray[currenty1][currenty2];
-		z = moveArray[currentz1][currentz2];
+			
+			if ((counter % 2) == 0 )
+			{
+				array2[currentx1][currentx2] = x;
+				array2[currenty1][currenty2] = y;
+				array2[currentz1][currentz2] = z;
 
-		cout << "X moved to ["<<currentx1<<"]["<<currentx2<<"]\n";
-		cout << "Y moved to ["<<currenty1<<"]["<<currenty2<<"]\n";
-		cout << "Z moved to ["<<currentz1<<"]["<<currentz2<<"]\n";
-
+				cout << "X position in array two is ["<<currentx1<<"]["<<currentx2<<"] at time " << counter<<"\n";
+				cout << "Y position in array two is ["<<currenty1<<"]["<<currenty2<<"] at time " << counter<<"\n";
+				cout << "Z position in array two is ["<<currentz1<<"]["<<currentz2<<"] at time " << counter<<"\n";
+				
+			
+			memset(array1, 0, sizeof(array1));
+				for(int i=0; i<8; i++)    //This loops on the rows.
+				{
+					for(int j=0; j<7; j++) //This loops on the columns
+					{
+						cout << array2[i][j]  << "  ";
+					}
+					cout << endl;
+				}
+			}
+			else
+			{
+				array1[currentx1][currentx2] = x;
+				array1[currenty1][currenty2] = y;
+				array1[currentz1][currentz2]= z;
+				
+				cout << "X position in array one is ["<<currentx1<<"]["<<currentx2<<"] at time " << counter<<"\n";
+				cout << "Y position in array one is ["<<currenty1<<"]["<<currenty2<<"] at time " << counter<<"\n";
+				cout << "Z position in array one is ["<<currentz1<<"]["<<currentz2<<"] at time " << counter<<"\n";
+				memset(array2, 0, sizeof(array1));
+				for(int i=0; i<8; i++)    //This loops on the rows.
+				{
+					for(int j=0; j<7; j++) //This loops on the columns
+					{
+						cout << array1[i][j]  << "  ";
+					}
+					cout << endl;
+				}
+			}
+		currentx1 ++;
+		currentx2 ++;
+		currenty1 ++;
+		currentz2 ++;
 		counter ++;
-		
+			
 	}
-
 }
 
 
@@ -69,7 +102,7 @@ void Movement::moveXYZ(int distance)
 int Movement::showPosition() const
 {
 	ofstream myfile;
-  myfile.open ("example.txt");
+  myfile.open ("test.txt");
   myfile << "X current location is ["<<currentx1<<"]["<<currentx2<<"]\n";
   myfile << "\nY current location is ["<<currenty1<<"]["<<currenty2<<"]\n";
   myfile << "\nZ current location is ["<<currentz1<<"]["<<currentz2<<"]\n";
@@ -78,6 +111,7 @@ int Movement::showPosition() const
 	cout <<"\nY current location is ["<<currenty1<<"]["<<currenty2<<"]\n";
 	cout <<"\nZ current location is ["<<currentz1<<"]["<<currentz2<<"]\n";
 	return currentx1;
+	
 	//return y;
 	//return z;
 }
